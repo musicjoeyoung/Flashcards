4d301564cfac8b54ee6b260ae516870f62827743
Joe Young <musicjoeyoung@gmail.com>
2023-11-08 11:58:30 -0500

    develop initial push

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Flashcard from '../../components/Flashcard/Flashcard'
+import CodeEditor from '../../components/CodeEditor/CodeEditor'
+
+const Home = () => {
+    return (
+        <div>
+            <Flashcard />
+            <CodeEditor />
+        </div>
+    )
+}
+
+export default Home
