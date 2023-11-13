10efa47ab285ffc259f02aea0db7fac5bd9bff61
Joe Young <musicjoeyoung@gmail.com>
2023-11-12 19:19:13 -0500

    about edit

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -12,9 +12,6 @@ const About = () => {
                 <li>Interactive Flashcards: Our extensive collection of JavaScript flashcards covers a wide range of terms and concepts, from the basics to advanced topics. Each flashcard is designed to help you learn and retain important information efficiently.</li>
                 <li> Code Editor: Practice what you learn in real-time with the integrated code editor right below the flashcards. Apply the concepts you've just studied, experiment, and see immediate results.</li>
                 <li>Examples: Every flashcard is accompanied by practical examples to illustrate how each term is used in real-world scenarios. This helps you understand the context and application of JavaScript concepts.</li>
-                <li>Progress Tracking: Keep track of your learning progress with our built-in progress tracker. Set goals, mark flashcards as mastered, and review your improvement over time.</li>
-                <li>Customizable Learning: Tailor your learning experience by focusing on specific topics or concepts that you want to master. Create custom flashcard decks to target your unique learning needs.</li>
-                <li>Offline Access: Don't worry about a reliable internet connection. CodeCards allows you to access your flashcards and practice offline, ensuring you can learn anytime, anywhere.</li>
             </ol>
             <h2>Who Can Benefit?</h2>
             <ul>
@@ -22,13 +19,13 @@ const About = () => {
                 <li>Intermediate Learners: Enhance your JavaScript proficiency by revisiting and reinforcing core concepts.</li>
                 <li>Advanced Developers: Even experienced developers can benefit from CodeCards by deepening their understanding of JavaScript's intricacies.</li>
             </ul>
-            <h2>How to Get Started:</h2>
+            {/*            <h2>How to Get Started:</h2>
             <ol>
                 <li>Download and install CodeCards on your device.</li>
                 <li>Create an account or sign in if you already have one.</li>
                 <li>Browse our extensive flashcard library, choose a deck, and start your learning journey.</li>
                 <li>Review, practice, and track your progress as you master JavaScript terminology and concepts.</li>
-            </ol>
+            </ol> */}
             <h2>Get Ahead in Your JavaScript Journey:</h2>
 
             <p>CodeCards is your key to unlocking the power of JavaScript. Start mastering the language today with our engaging and interactive flashcards, code editor, and real-world examples. Whether you're preparing for an interview, building a project, or expanding your skill set, we're here to support your JavaScript learning goals. Join our community of learners and take your coding skills to the next level!</p>
