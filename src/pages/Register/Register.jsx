262b9831a2daa896a6bde6e36e1c88a6896dd3f9
Joe Young <musicjoeyoung@gmail.com>
2024-04-12 10:37:37 -0400

    main --> develop

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,11 @@
+import "./Register.scss"
+import { useState } from "react"
+import axios from "axios"
+
+const Register = () => {
+    return (
+        <div>Register</div>
+    )
+}
+
+export default Register
